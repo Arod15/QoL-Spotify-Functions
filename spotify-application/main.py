df6c58d43062e2165b7fc8e1bb9e1c167ff12214
arod <arod1198@gmail.com>
2018-08-24 13:59:17 -0400

    Made loop optimizations and hid sensitive information.

--- spotify-application/main.py
+++ spotify-application/main.py
@@ -1,17 +1,10 @@
-# from spotipy import oauth2
 from spotipy import util
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy.oauth2 import SpotifyOAuth
+# from spotipy.oauth2 import SpotifyClientCredentials
+# from spotipy.oauth2 import SpotifyOAuth
 import spotipy
+import config
 
-user_library_read = 'user-library-read'
-playlist_modify_public = 'playlist-modify-public'
-scope = user_library_read + ' ' + playlist_modify_public
-user = 'ar15student'
-client_id = '6f0e1625137a45bab876b91906a38f01'
-client_secret = 'bc349d6984e140ef98639e1c77e8ba0b'
-
-token = util.prompt_for_user_token(user,scope=scope,client_id=client_id,client_secret=client_secret, redirect_uri='http://localhost/')
+token = util.prompt_for_user_token(config.user,scope=config.scope,client_id=config.client_id,client_secret=config.client_secret, redirect_uri='http://localhost/')
 spotify = spotipy.Spotify(auth=token)
 
 library = {}
@@ -24,7 +17,8 @@ def extract_library():
         extract_artist_song(library_results)
         offset += 50
 def extract_artist_song(response):
-    for i in range(len(response['items'])):
+    song_batch_len = len(response['items'])
+    for i in range(song_batch_len):
         song_id = response['items'][i]['track']['id']
         song = response['items'][i]['track']['name']
         library[song] = song_id
@@ -39,7 +33,7 @@ def clean_playlists():
     num_playlists = len(playlist_ids)
     for i in range(num_playlists):
         song_ids = []
-        results = spotify.user_playlist_tracks(user, playlist_id=playlist_ids[i], limit=100)
+        results = spotify.user_playlist_tracks(config.user, playlist_id=playlist_ids[i], limit=100)
         for dic in results['items']:                              
             song_id = dic['track']['id']
             song_name = dic['track']['name']
@@ -48,7 +42,7 @@ def clean_playlists():
                 song_ids.append(song_id)
         print(song_ids)
         if len(song_ids) != 0:
-            spotify.user_playlist_remove_all_occurrences_of_tracks(user, playlist_id=playlist_ids[i], tracks=song_ids)
+            spotify.user_playlist_remove_all_occurrences_of_tracks(config.user, playlist_id=playlist_ids[i], tracks=song_ids)
         print(song_ids)
 extract_library()
 clean_playlists()
